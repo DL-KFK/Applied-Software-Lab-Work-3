d1d3d904182787f1a986801fae0e1f5fa2677f1d
DL-KFK <dl.kpt.sumdu@gmail.com>
2025-11-17 23:21:22 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+import requests
+import os
+
+app = Flask(__name__)
+
+BOT_TOKEN = os.getenv(7636102794:AAFYBeZS6u84dcRlQFcxFy270q2wD8uZpPc)
+CHAT_ID = os.getenv(KFK_PPZ_Bot)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/submit", methods=["POST"])
+def submit():
+    fullname = request.form["fullname"]
+
+    message = f"🔔 Нове повідомлення!\nСтудент: {fullname}"
+
+    requests.get(f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
+                 params={"chat_id": CHAT_ID, "text": message})
+
+    return "Дякую! Дані відправлено викладачу."
