4d4c82998e4aab44c73c1d10432545c5a28998de
DL-KFK <dl.kpt.sumdu@gmail.com>
2025-11-17 23:44:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -15,4 +15,4 @@ def submit_name():
     return jsonify({"message": f"Дякую, {name}, дані отримано!"})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
